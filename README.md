5af1c8ae359a02e9df221f1041222f1bdd57798a
vd4re <vd4re@protonmail.ch>
2021-10-14 23:56:31 -0400

    Add api_call macro

---
+++ README.md
@@ -0,0 +1,5 @@
+# Windows API hashing in Rust
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![crates.io v0.1.0](https://img.shields.io/crates/v/winapi-hash)](https://crates.io/crates/obfstr)
+[![docs.io v0.1.0](https://img.shields.io/docsrs/winapi-hash)](https://docs.rs/crate/winapi-hash/)
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,3 +6,23 @@ macro_rules! to_va {
         ($base as winapi::shared::minwindef::LPBYTE).offset($offset as isize)
     };
 }
+
+// I forget where I got this trick from, will credit when found
+#[macro_export]
+macro_rules! infer_type {
+    ($($tt:tt)*) => {
+        _
+    };
+}
+
+// Once const generics mature to allow const fn in traits, this macro can be cleaned up to remove the $alg:ty
+#[macro_export]
+macro_rules! api_call {
+    ($resolver:expr, $alg:ty, $mod:literal -> $fn:literal ($($arg:expr),* $(,)?)) => {{
+        const FN_HASH: [u8; <$alg>::OUTPUT_SIZE] = <$alg>::digest($fn.as_bytes());
+        let function: fn($(crate::infer_type!($arg)),*) -> _ = unsafe {
+            std::mem::transmute($resolver.resolve_fn($mod, FN_HASH).unwrap())
+        };
+        function($($arg),*)
+    }};
+}
